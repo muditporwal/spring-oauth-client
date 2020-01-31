f8c1c6c9015e1e869d4122af7a4c1f0a808ac672
mudit <9289168+muditporwal@users.noreply.github.com>
2020-02-01 03:58:31 +0800

    Empty Spring Application Added

---
+++ src/main/java/com/tryme/spring/web/oauth/OauthApplication.java
@@ -0,0 +1,13 @@
+package com.tryme.spring.web.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthApplication.class, args);
+	}
+
+}
